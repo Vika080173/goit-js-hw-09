d2531de05f7199cbbe1c3dd1af0a70e54f335e5f
Vika080173 <151369286+Vika080173@users.noreply.github.com>
2024-03-30 09:47:19 +0200

    add gallery

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,25 @@
-console.log('Form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+populateTextarea();
+
+form.addEventListener('submit', handleSubmit);
+textarea.addEventListener('input', onTextareaInput);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(event) {
+  const messege = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, messege);
+}
+function populateTextarea() {
+  const savedMessege = localStorage.getItem(STORAGE_KEY);
+  if (savedMessege) {
+    textarea.value = savedMessege;
+  }
+}
